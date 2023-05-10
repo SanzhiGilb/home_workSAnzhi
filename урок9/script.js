f5d6c52b23881e2d5938e722eb4463216e6c02b7
SanzhiGilb <anatoliysanzhiev2@gmail.com>
2023-05-10 21:18:32 +0300

    Add massive

--- урок9/script.js
+++ урок9/script.js
@@ -1,8 +1,34 @@
-let name = 'Dave'
+let soap= {
 
-let user = {
-	name, // то же самое что и name: name,
-	age: 30
+    ing1: 'potato',
+    ing2: 'luc',
+    ing3: 'meat',
+    ing4: 'water',
+    ing5: 'spagetti'
 };
+let eggs= {
 
-alert(user.name); // 'Dave'
+    ing1: 'eggs',
+    ing2: 'oil'
+};
+let buuzy={
+
+    ing1: 'dough',
+    ing2: 'minced meat'
+};
+let menu = [];
+
+menu.push('buuzy (35min), 70rub za sht');
+menu.push('eggs (10min), 100rub');
+menu.push('soap (30min), 250rub ');
+
+menu.pop();
+delete soap.ing4;
+menu.unshift('soap (35min), 250rub');
+
+
+let cash = 'Cash =';
+let info = (250+100+70)*365;
+
+console.log(menu);
+console.log(cash+info);
